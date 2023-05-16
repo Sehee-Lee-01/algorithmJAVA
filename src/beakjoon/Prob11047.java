5e6062dcca6a250b6b793bab97fd9c390c0b7e69
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-17 02:41:15 +0900

    sol: 11047

---
+++ src/beakjoon/Prob11047.java
@@ -0,0 +1,30 @@
+package beakjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class Prob11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] coins = new int[N];
+        for (int i=0; i<N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            coins[i] = n;
+        }
+        
+        int count = 0; 
+        for (int i=N-1; i>=0; i--) {
+            if (K >= coins[i]) {
+                count += K / coins[i];
+                K %= coins[i];
+            }
+            if (K == 0) break;
+        }
+        System.out.println(count);
+    }
+}
