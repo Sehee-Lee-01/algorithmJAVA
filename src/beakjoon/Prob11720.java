ac16311ca010d978d267b1cdf67ac5a7fc0f4c8c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-26 02:12:03 +0900

    error: 11720 코드 수정

--- src/beakjoon/Prob11720.java
+++ src/beakjoon/Prob11720.java
@@ -6,13 +6,13 @@ import java.io.InputStreamReader;
 public class Prob11720 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 안써도 된다.
         long sum = 0;
         String nums = br.readLine();
-        char[] num = nums.toCharArray() // String -> char[]
-        for (int i = 0; i < nums.length(); i++) {
+        char[] num_list = nums.toCharArray(); // String -> char[]
+        for (char num: num_list) {
             // sum += nums.charAt(i) - '0';
-            sum += num[i] - '0';
+            sum += num - '0';
         }
         System.out.println(sum);
     }
