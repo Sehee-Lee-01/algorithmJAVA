0b6dccdf130cbcdf89a1b3d068d3ca7898b60764
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-26 22:41:22 +0900

    refactor: 1940 성능 개선

--- src/beakjoon/Prob1940.java
+++ src/beakjoon/Prob1940.java
@@ -14,26 +14,24 @@ public class Prob1940 {
         int N = Integer.parseInt(br.readLine()), M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         int[] m = new int[N];
-        boolean[] u = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            m[i] = Integer.parseInt(st.nextToken());
-            u[i] = false;
-        }
+        for (int i = 0; i < N; i++) m[i] = Integer.parseInt(st.nextToken());
         Arrays.sort(m);
 
         int st = 0, ed = 1, cnt = 0, sum = 0;
         for (int i = 0; i < N; i++) {
-            if (u[i])
+            if (m[i] > M)
+                break;
+            if (m[i] == 0)
                 continue;
             for (int j = i + 1; j < N; j++) {
-                if (u[j])
+                if (m[j] == 0)
                     continue;
                 if (m[i] + m[j] > M)
                     break;
                 if (m[i] + m[j] == M) {
                     cnt++;
-                    u[i] = true;
-                    u[j] = true;
+                    m[i] = 0;
+                    m[j] = 0;
                     break;
                 }
             }
