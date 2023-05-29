d2ae0ce01f04b154779d8a03b2f643c84f273177
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-29 17:23:02 +0900

    sol: 1920

---
+++ src/beakjoon/Prob1920.java
@@ -0,0 +1,53 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Prob1920 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> Nlist = new ArrayList<>();
+        for (int i = 0; i < N; i++)
+            Nlist.add(Integer.parseInt(st.nextToken()));
+        Nlist.sort(null);
+
+        st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> Mlist = new ArrayList<>();
+        for (int i = 0; i < M; i++)
+            Mlist.add(Integer.parseInt(st.nextToken()));
+
+        for (int i = 0; i < M; i++) {
+            sb.append(binarySearch(Nlist, Mlist.get(i))).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static int binarySearch(ArrayList<Integer> list, int target) {
+        int start = 0, end = list.size() - 1, mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (list.get(mid) == target)
+                return 1;
+            else if (list.get(mid) > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return 0;
+    }
+}
