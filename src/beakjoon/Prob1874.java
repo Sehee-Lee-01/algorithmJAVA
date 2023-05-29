af2d627b4f714423371876c65907bad32c4b43c7
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-29 14:22:57 +0900

    sol:1874

---
+++ src/beakjoon/Prob1874.java
@@ -0,0 +1,41 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Prob1874 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int[] stack = new int[100000];
+    static int top = -1;
+    static int recentPushNum = 0;
+
+    static void push() {
+        stack[++top] = ++recentPushNum;
+        sb.append("+").append("\n");
+    }
+
+    static int pop() {
+        if (top == -1)
+            return -1;
+        sb.append("-").append("\n");
+        return stack[top--];
+    }
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            while (recentPushNum < arr[i]) {
+                push();
+            }
+            if (pop() != arr[i]) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println(sb);
+    }
+}
