4565b5254facecba4433909c4489c454e4062982
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-05-29 15:48:59 +0900

    refactor: 2178 코드 정리
    - 다만 시간이 더 늘어났다.

--- src/beakjoon/Prob2178.java
+++ src/beakjoon/Prob2178.java
@@ -1,30 +1,37 @@
 package beakjoon;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Prob2178 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
     static void bfs(int[][] map, int N, int M) {
-        ArrayList<int[]> queue = new ArrayList<>();
+        Queue<int[]> queue = new LinkedList<>();
         // 이전 값은 사실 저장할 필요 없다.(이전 값으로 순회할 경우에는 무한 루프 발생 가능)
         // cnt 수대로 map에 각 칸에 접근한 최소 거리를 저장하자.
-        int[] start = { 0, 0, 1 }; 
+        int[] start = { 0, 0, 1 };
         queue.add(start);
         while (!queue.isEmpty()) {
-            int[] front = queue.remove(0);
-            // System.out.println(front[0]);
+            int[] front = queue.poll();
             int y = front[0];
             int x = front[1];
             int cnt = front[2];
 
-            if ((map[y][x] != 1) && (map[y][x] <= cnt) && !(y == 0 && x == 0)) continue; 
+            if ((map[y][x] != 1) && (map[y][x] <= cnt) && !(y == 0 && x == 0))
+                continue;
 
             map[y][x] = cnt;
 
-            if (y == N - 1 && x == M - 1) 
+            if (y == N - 1 && x == M - 1)
                 break;
-            
-                // 오른
+
+            // 오른
             if (x < M - 1 && map[y][x + 1] > 0) {
                 int[] temp = { y, x + 1, cnt + 1 };
                 queue.add(temp);
@@ -48,16 +55,15 @@ public class Prob2178 {
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int[][] map = new int[N][M];
 
         for (int i = 0; i < N; i++) {
-            String str = br.readLine();
+            char[] str = br.readLine().toCharArray();
             for (int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j) - '0';
+                map[i][j] = str[j] - '0';
             }
         }
         bfs(map, N, M);
