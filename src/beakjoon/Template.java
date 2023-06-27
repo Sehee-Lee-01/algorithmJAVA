090de94a1c4f519ab9aeb409238109afe31d650b
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-06-27 19:57:53 +0900

    add template

---
+++ src/beakjoon/Template.java
@@ -0,0 +1,17 @@
+package beakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Template {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
