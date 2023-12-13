2a089e0ea0fa5707dbce5ef6690c2e07d0cf05b9
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-13 22:23:31 +0900

    sol: RemoveDuplicateLetters 스택 풀이

--- src/leetcode/RemoveDuplicateLetters.java
+++ src/leetcode/RemoveDuplicateLetters.java
@@ -1,32 +1,48 @@
 package leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class RemoveDuplicateLetters {
-
     public String removeDuplicateLetters(String s) {
-        Set<Character> sortedSet = getSortedString(s);
-        for (char c : sortedSet) {
-            String subStr = s.substring(s.indexOf(c));
-            if (sortedSet.equals(getSortedString(subStr))) {
-                return c + removeDuplicateLetters(subStr.replaceAll(String.valueOf(c), ""));
+        Map<Character, Integer> counter = new HashMap<>();
+        Set<Character> seen = new HashSet<>();
+        Deque<Character> stack = new ArrayDeque<>();
+
+        char[] chars = s.toCharArray();
+        setCharCount(chars, counter);
+
+        for (char c : chars) {
+            counter.put(c, counter.get(c) - 1);
+            if (seen.contains(c))
+                continue;
+
+            while (!stack.isEmpty() && stack.peek() > c && counter.get(stack.peek()) > 0) {
+                seen.remove(stack.pop());
             }
+
+            stack.push(c);
+            seen.add(c);
         }
-        return "";
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+        return sb.toString();
     }
 
-    private Set<Character> getSortedString(String s) {
-        Set<Character> res = new TreeSet<>((c1, c2) -> c1 - c2);
-
-        for (char c : s.toCharArray()) {
-            res.add(c);
+    private void setCharCount(char[] chars, Map<Character, Integer> counter) {
+        for (char c : chars) {
+            counter.put(c, counter.getOrDefault(c, 0) + 1);
         }
-
-        return res;
     }
 }
